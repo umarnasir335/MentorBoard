70f7f482fb6262d89abbaa78b1be319f83a3dc58
umarnasir335 <umarnasir335@gmail.com>
2019-11-24 23:06:07 -0800

    First inclusion of React BigCalendar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import './App.css';
 import Navbar from './Components/Navbar';
 import Activities from './Components/Activities';
+import Schedule from './Components/Schedule';
 //import Messages from './Messages';
 //import Schedule from './Schedule';
 //import Tutors from './Tutors';
@@ -18,6 +19,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1> MentorBoard</h1>
+      <Schedule></Schedule>
       <Switch>
         <Route exact path="/" component={Navbar}/>
         </Switch>
@@ -25,6 +27,11 @@ function App() {
       <div className = "App-body">
         <Switch>
         <Route path="/activities" component={Activities}/>  
+        </Switch>
+      </div>
+      <div>
+        <Switch>
+        <Route path="/schedule" component={Schedule}/>  
         </Switch>
       </div>
     </div>
--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -1,13 +1,123 @@
 import React, { Component } from 'react'
+import { render } from 'react-dom';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+//import './style.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
 
 export class Schedule extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Scheduler/Calendar!!!</h3>
-            </div>
-        )
-    }
+    constructor(){
+        super();
+        const now = new Date();
+const events = [
+    {
+        id: 0,
+        title: 'All Day Event very long title',
+        allDay: true,
+        start: new Date(2019, 3, 0),
+        end: new Date(2019, 3, 1),
+    },
+    {
+        id: 1,
+        title: 'Long Event',
+        start: new Date(2019, 3, 7),
+        end: new Date(2019, 3, 10),
+    },
+
+    {
+        id: 2,
+        title: 'DTS STARTS',
+        start: new Date(2019, 2, 13, 0, 0, 0),
+        end: new Date(2019, 2, 20, 0, 0, 0),
+    },
+
+    {
+        id: 3,
+        title: 'DTS ENDS',
+        start: new Date(2019, 10, 6, 0, 0, 0),
+        end: new Date(2019, 10, 13, 0, 0, 0),
+    },
+
+    {
+        id: 4,
+        title: 'Some Event',
+        start: new Date(2019, 3, 9, 0, 0, 0),
+        end: new Date(2019, 3, 10, 0, 0, 0),
+    },
+    {
+        id: 5,
+        title: 'Conference',
+        start: new Date(2019, 11, 11),
+        end: new Date(2019, 11, 13),
+        desc: 'Big conference for important people',
+    },
+    {
+        id: 6,
+        title: 'Meeting',
+        start: new Date(2019, 11, 12, 10, 30, 0, 0),
+        end: new Date(2019, 11, 12, 12, 30, 0, 0),
+        desc: 'Pre-meeting meeting, to prepare for the meeting',
+    },
+    {
+        id: 7,
+        title: 'Lunch',
+        start: new Date(2019, 11, 12, 12, 0, 0, 0),
+        end: new Date(2019, 11, 12, 13, 0, 0, 0),
+        desc: 'Power lunch',
+    },
+    {
+        id: 8,
+        title: 'Meeting',
+        start: new Date(2019, 11, 12, 14, 0, 0, 0),
+        end: new Date(2019, 11, 12, 15, 0, 0, 0),
+    },
+    {
+        id: 9,
+        title: 'Happy Hour',
+        start: new Date(2019, 11, 12, 17, 0, 0, 0),
+        end: new Date(2019, 11, 12, 17, 30, 0, 0),
+        desc: 'Most important meal of the day',
+    },
+    {
+        id: 10,
+        title: 'Meeting with team',
+        start: new Date(new Date().setHours(new Date().getHours() - 3)),
+        end: new Date(new Date().setHours(new Date().getHours() + 3)),
+    },
+    {
+        id: 11,
+        title: 'Current Point in Time',
+        start: now,
+        end: now, 
+    },
+    ]
+
+    this.state = {
+        name: 'React',
+        events
+      };
+
+}
+render() {
+    return (
+      <div>
+        <p>
+          A test for the React Big Calendar.
+        </p>
+        <div style={{ height: '500pt'}}>
+          <Calendar
+            events={this.state.events}
+            startAccessor="start"
+            endAccessor="end"
+            defaultDate={moment().toDate()}
+            localizer={localizer}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Schedule
